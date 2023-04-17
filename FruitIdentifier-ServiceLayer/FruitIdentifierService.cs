25fc021c32d63e69689d4f6d07eec40bcc9c5cb1
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2023-04-17 15:07:13 +0200

    Add project files.

---
+++ FruitIdentifier-ServiceLayer/FruitIdentifierService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitIdentifier_ServiceLayer
+{
+    public class FruitIdentifierService : IFruitIdentifierService
+    {
+
+    }
+}
