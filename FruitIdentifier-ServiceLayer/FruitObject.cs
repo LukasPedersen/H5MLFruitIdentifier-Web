fa2d89be05a393f4176dad783437cc3afed0dd1b
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2023-04-26 11:55:35 +0200

    Added better UI Added Fruit nutritions

---
+++ FruitIdentifier-ServiceLayer/FruitObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FruitIdentifier_ServiceLayer
+{
+    public class Nutritions
+    {
+        public int Calories { get; set; }
+        public double Fat { get; set; }
+        public double Sugar { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Protein { get; set; }
+    }
+    public class FruitObject
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Family { get; set; }
+        public string Order { get; set; }
+        public string Genus { get; set; }
+        [JsonPropertyName("nutritions")]
+        public Nutritions FruitNutritions { get; set; } = new();
+    }
+}
